138d47012831bf5cc9d9cb622df75a36438e4d24
phamtiendat18 <phamtiendat2002nd@gmail.com>
2024-11-08 14:39:47 +0700

    Update code

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const { register, login } = require('../controllers/authController');
+const express = require("express");
+const { register, login } = require("../controllers/authController");
 const router = express.Router();
 
-router.post('/p/register', register);
-router.post('/p/login', login);
-router.post('/a/login', login);
-router.post('/s/register', register);
-router.post('/s/login', login);
-router.get('/test', (req,res)=>{
-    return res.send("abc")
-})
+router.post("/p/register", register);
+router.post("/p/login", login);
+router.post("/a/login", login);
+router.post("/s/register", register);
+router.post("/s/login", login);
+router.get("/test", (req, res) => {
+  return res.status(200).json({ message: "test" });
+});
 
 module.exports = router;
