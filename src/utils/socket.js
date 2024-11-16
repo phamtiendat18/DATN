4defce49e3159377f7caa799e3f44e7ab67d6e75
phamtiendat18 <phamtiendat2002nd@gmail.com>
2024-11-16 23:35:44 +0700

    Update code of video call

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,7 @@
 const { Server } = require("socket.io");
+
 let io;
+
 const initSocket = (server) => {
   io = new Server(server, {
     cors: {
@@ -8,33 +10,46 @@ const initSocket = (server) => {
     },
   });
 
+  // Lắng nghe sự kiện kết nối
   io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
 
-    // Lắng nghe sự kiện gọi video
-    socket.on("start-video-call", (data) => {
-      io.emit("video-call-started", data); // Phát sóng cuộc gọi mới
-    });
+    // Lắng nghe và xử lý các sự kiện từ client
+    registerVideoCallEvents(socket);
 
-    // Lắng nghe sự kiện người tham gia cuộc gọi
-    socket.on("join-video-call", (data) => {
-      io.emit("user-joined", data); // Phát sóng người tham gia
-    });
-
-    // Kết thúc cuộc gọi
-    socket.on("end-video-call", (data) => {
-      io.emit("video-call-ended", data); // Phát sóng cuộc gọi kết thúc
-    });
-
+    // Lắng nghe sự kiện disconnect
     socket.on("disconnect", () => {
-      console.log("A user disconnected");
+      console.log(`User disconnected: ${socket.id}`);
     });
   });
 };
-// Hàm truy cập đối tượng io
+
+const registerVideoCallEvents = (socket) => {
+  // Sự kiện bắt đầu cuộc gọi video
+  socket.on("start-video-call", (data) => {
+    console.log("Video call started:", data);
+    io.emit("video-call-started", data); // Gửi thông báo đến tất cả client
+  });
+
+  // Sự kiện tham gia cuộc gọi video
+  socket.on("join-video-call", (data) => {
+    console.log("User joined video call:", data);
+    io.emit("user-joined", data); // Gửi thông báo đến tất cả client
+  });
+
+  // Sự kiện kết thúc cuộc gọi video
+  socket.on("end-video-call", (data) => {
+    console.log("Video call ended:", data);
+    io.emit("video-call-ended", data); // Gửi thông báo đến tất cả client
+  });
+};
+
+/**
+ * Trả về instance của Socket.IO
+ */
 const getIoInstance = () => {
   if (!io) {
-    throw new Error("Socket.io chưa được khởi tạo!");
+    throw new Error("Socket.IO chưa được khởi tạo!");
   }
   return io;
 };
