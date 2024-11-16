dd507dda24d5c3c59b973ea09943e7c85cee39f4
phamtiendat18 <phamtiendat2002nd@gmail.com>
2024-11-17 00:33:28 +0700

    uodate code

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,12 +3,7 @@ const { Server } = require("socket.io");
 let io;
 
 const initSocket = (server) => {
-  io = new Server(server, {
-    cors: {
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST"],
-    },
-  });
+  io = new Server(server);
 
   // Lắng nghe sự kiện kết nối
   io.on("connection", (socket) => {
