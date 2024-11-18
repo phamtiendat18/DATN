a52d26102ba0736cc3182584c166296bd80a9eaa
phamtiendat18 <phamtiendat2002nd@gmail.com>
2024-11-18 16:41:43 +0700

    Update code genarate token

--- src/utils/genarateAccessToken.js
+++ src/utils/genarateAccessToken.js
@@ -4,21 +4,33 @@ require("dotenv").config();
 const apiKeySid = process.env.STRINGEE_SID_KEY;
 const apiKeySecret = process.env.STRINGEE_SECRET_KEY;
 
-const generateAccessToken = (useId) => {
+const generateAccessToken = (userId, roomId) => {
   var now = Math.floor(Date.now() / 1000);
-  var exp = now + 86400;
+  var exp = now + 3600 * 24;
 
   var header = { cty: "stringee-api;v=1" };
-  var payload = {
-    jti: apiKeySid + "-" + now,
-    iss: apiKeySid,
-    exp: exp,
-    useId: useId,
-  };
+  var payload;
+  if (roomId) {
+    payload = {
+      jti: apiKeySid + "-" + now,
+      iss: apiKeySid,
+      exp: exp,
+      userId: `${userId}`,
+      roomId: `${roomId}`,
+    };
+  } else {
+    payload = {
+      jti: apiKeySid + "-" + now,
+      iss: apiKeySid,
+      exp: exp,
+      userId: `${userId}`,
+    };
+  }
 
   var token = jwt.sign(payload, apiKeySecret, {
     algorithm: "HS256",
     header: header,
+    noTimestamp: true,
   });
   return token;
 };
